c5d487e675ae53a029a0a054a6b5016537a776f4
Ben <b.k.tidswell@gmail.com>
2020-09-21 12:50:29 -0400

    recent update

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN apt-get -y update && apt-get -y install gcc
 
 WORKDIR /
 COPY checkpoint /checkpoint
+COPY models /models
+COPY text_files /text_files
 
 # Copy local code to the container image.
 #ENV APP_HOME /app
@@ -13,8 +15,9 @@ COPY checkpoint /checkpoint
 # Make changes to the requirements/app here.
 # This Dockerfile order allows Docker to cache the checkpoint layer
 # and improve build times if making changes.
-RUN pip3 --no-cache-dir install tensorflow==1.15.2 gpt-2-simple starlette uvicorn ujson
+RUN pip3 --no-cache-dir install tensorflow==1.15.2 gpt-2-simple starlette uvicorn ujson google-cloud-logging
 COPY app.py /
+COPY daily_training.py /
 
 # Clean up APT when done.
 RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- daily_training.py
+++ daily_training.py
@@ -1,33 +1,41 @@
 import gpt_2_simple as gpt2
 from datetime import datetime
 from random import *
-import shutil, os, glob, sys
+import shutil, os, glob
+from google.cloud import logging
 
-def print_to_stderr(*a): 
-  
-    # Here a is the array holding the objects 
-    # passed as the arguement of the function 
-    print(*a, file = sys.stderr) 
+logging_client = logging.Client()
+# The name of the log to write to
+log_name = 'my-log'
+# Selects the log to write to
+logger = logging_client.logger(log_name)
+
  
 def moveAllFilesinDir(srcDir, dstDir):
 	# Check if both the are directories
+
 	if os.path.isdir(srcDir) and os.path.isdir(dstDir):
 		# Iterate over all the files in source directory
 		for filePath in glob.glob(srcDir + '/*'):
 			# Move each file to destination Directory
 			shutil.move(filePath, dstDir)
-		print("Finished moving "+srcDir+" to "+ dstDir)
+		text = "Finished moving "+srcDir+" to "+ dstDir
 	else:
-		print("srcDir & dstDir should be Directories")
+		text = "srcDir & dstDir should be Directories"
+
+	print(text)
+	logger.log_text(text)
 
 def trainNewModel():
 			
 	#Step 1: Check if it has been 24 hours
 
+	logger.log_text("Started Training") 
+
 	textfiles = ["Emily_D_poems.txt","SeaChute_Bisuness.txt","Magnus_Statements.txt"]
 
 	#Step 2: Select the text file
-	file_name = textfiles[randint(0,len(textfiles)-1)]
+	file_name = "text_files/"+textfiles[randint(0,len(textfiles)-1)]
 	RUN_NAME = "run1"
 	MODEL_NAME= "run1"
 
@@ -46,33 +54,52 @@ def trainNewModel():
 
 	#Real Step 3: Delete old model
 
+	if cwd == "/":
+		cwd = ""
+
 	shutil.rmtree(cwd+"/models/run1") 
+	os.mkdir(cwd+"/models/run1")
 
 	#Step 4: Move the run1 checkpoint into the models
-	moveAllFilesinDir(cwd+"/checkpoint/run1",cwd+"/models/run1")
+	moveAllFilesinDir(cwd+"/checkpoint/run1/",cwd+"/models/run1")
 
 	#Step 4a: Delete the old run1 folder
 	os.rmdir(cwd+"/checkpoint/run1")
 
+	logger.log_text("Moved Files")
+
+	files = os.listdir("/")
+	logger.log_text(str(files)) 
+
+	files2 = os.listdir(cwd+"models/run1")
+	files3 = os.listdir(cwd+"checkpoint")
+
+	logger.log_text(str(files2)) 
+	logger.log_text(str(files3)) 
+
 	#Step 5: Train the model
 	sess = gpt2.start_tf_sess()
 
-	gpt2.finetune(sess,
-	              dataset=file_name,
-	              model_name= MODEL_NAME,
-	              steps=10,
-	              restore_from='fresh',
-	              run_name=RUN_NAME,
-	              print_every=10,
-	              sample_every=10,
-	              save_every=10,
-	              learning_rate = 1e-5,
-	              overwrite=False
-	              )
+	try:
+		gpt2.finetune(sess,
+		              dataset=file_name,
+		              model_name= MODEL_NAME,
+		              steps=10,
+		              restore_from='fresh',
+		              run_name=RUN_NAME,
+		              print_every=10,
+		              sample_every=10,
+		              save_every=10,
+		              learning_rate = 1e-5,
+		              overwrite=False,
+		              )
+	except:
+		logger.log_text("Got here I guess") 
 
 	#Step 6: Log this training sessions
-	print_to_stderr("%s : Trained with %s \n" % (now.strftime("%m/%d/%Y %H:%M:%S"), file_name)) 
+	text = "%s : Trained with %s \n" % (now.strftime("%m/%d/%Y %H:%M:%S"), file_name)
+	logger.log_text(text) 
+	print('Logged: {}'.format(text))
 	# f = open("training_log.txt", "a")
 	# f.write("%s : Trained with %s \n" % (now.strftime("%m/%d/%Y %H:%M:%S"), file_name))
 	# f.close()
-
